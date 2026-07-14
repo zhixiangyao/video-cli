94fb729095c664b502d8900253d5ffe4da05b997
zhixiangyao <blueskyzx@icloud.com>
2026-07-14 09:45:30 +0800

    init

---
+++ source/app.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'ink'
+
+type Props = {
+  name: string | undefined
+}
+
+export default function App({ name = 'Stranger' }: Props) {
+  return (
+    <Text>
+      Hello, <Text color="green">{name}</Text>
+    </Text>
+  )
+}
